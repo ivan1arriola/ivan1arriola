98e71d41e741be5bef76327de808a0340344912f
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-09-13 14:58:55 -0300

    habilidades

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -6,7 +6,7 @@ function Habilidades() {
     <h2>Habilidades</h2>
     <div className='container'>
         <ListGroup>
-        <ListGroup.Item variant="success" className='text'><p>Programar en C, Pascal, R, Javascript y Java</p></ListGroup.Item>
+        <ListGroup.Item variant="success" className='text'><p>C++ - Pascal - R - Javascript - Java </p></ListGroup.Item>
         <ListGroup.Item variant="info" className='text'><p>Buscar por internet absolutamente todo lo que no sé</p></ListGroup.Item>
         <ListGroup.Item variant="warning" className='text'><p>Conocimientos de Illustrator</p></ListGroup.Item>
         <ListGroup.Item variant="primary" className='text'><p>Conocimientos de React</p></ListGroup.Item>
